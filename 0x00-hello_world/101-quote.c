c0b2e869bddce18fd13a7e0b1bc219a1921be277
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-02-16 19:01:21 -0800

    Commiting the advance project

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that output to the standard error
+ *
+ * Return: This time we return an error 1.
+*/
+
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19,");
+		return (1);
+}
+
