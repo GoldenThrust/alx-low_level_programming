6ddb128e757cc78a0f1b8dccd334bc4ed652fca9
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-02-16 18:18:17 -0800

    Committing the mandatory project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ *
+ * Description: A program that output using put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A program output the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+
+int main(void)
+{
+	/* sizeof evaluate the size of a variable*/
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of a int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
+
