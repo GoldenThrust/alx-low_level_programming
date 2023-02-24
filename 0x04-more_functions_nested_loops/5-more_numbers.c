413ccfc7d3d821507b022967de885973bcbe0c64
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-02-23 18:12:10 -0800

    More work on function and llop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
