4b3e98dff4e872d2f5838eb64f30981dbbe98e63
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-03-22 16:49:36 -0700

    Still pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,13 @@
-#include "function_pointers.h"
 
 /**
- * print_name - print name
+ * print_name - function to print name
  *
  * @name: name to print
  * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
+	if (name && f)
 		f(name);
 }
