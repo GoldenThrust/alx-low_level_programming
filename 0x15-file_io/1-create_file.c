642690416cf59bd68540fd3ae53c24d76068fde1
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-04-10 09:58:41 -0700

    File input and output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a new file or overwrites
+ * the contents of an existing file.
+ *
+ * @filename: The name of the file to create or overwrite.
+ * @text_content: The text to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    size_t text_len, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		text_len = 0;
+        
+		while (text_content[text_len] != '\0')
+			text_len++;
+
+		bytes_written = write(fd, text_content, text_len);
+
+		if (bytes_written == (size_t)-1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
