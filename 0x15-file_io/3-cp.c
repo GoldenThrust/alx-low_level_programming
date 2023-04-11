48963814128b95c4ee77190aa30be01b4a4337f8
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-04-10 17:50:40 -0700

    File input and output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,33 +16,33 @@ int copy_file(int from_fd, int to_fd);
 
 int main(int argc, char **argv)
 {
-        int from_fd, to_fd;
+	int from_fd, to_fd;
 
-        if (argc != 3)
-        {
-                dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-                exit(97);
-        }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
 
-        from_fd = open_file(argv[1], O_RDONLY, 0);
+	from_fd = open_file(argv[1], O_RDONLY, 0);
 
-        to_fd = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-                        S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	to_fd = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
-        copy_file(from_fd, to_fd);
+	copy_file(from_fd, to_fd);
 
-        if (close(from_fd) == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
-                close(to_fd);
-                exit(100);
-        }
-        if (close(to_fd) == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
-                exit(100);
-        }
-        return (0);
+	if (close(from_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+		close(to_fd);
+		exit(100);
+	}
+	if (close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+		exit(100);
+	}
+	return (0);
 }
 
 /**
@@ -57,20 +57,20 @@ int main(int argc, char **argv)
 
 int open_file(const char *file_name, int flags, mode_t mode)
 {
-        int fd;
+	int fd;
 
-        fd = open(file_name, flags, mode);
+	fd = open(file_name, flags, mode);
 
-        if (fd == -1)
-        {
-                if (flags & O_RDONLY)
-                        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
-                else if (flags & O_WRONLY)
-                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
-                exit(errno);
-        }
+	if (fd == -1)
+	{
+		if (flags & O_RDONLY)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+		else if (flags & O_WRONLY)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+		exit(99);
+	}
 
-        return fd;
+	return (fd);
 }
 
 /**
@@ -84,25 +84,25 @@ int open_file(const char *file_name, int flags, mode_t mode)
 
 int copy_file(int from_fd, int to_fd)
 {
-        int bytes_read, bytes_written;
-        char buffer[BUFFER_SIZE];
+	int bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-        while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
-        {
-                bytes_written = write(to_fd, buffer, bytes_read);
+	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
 
-                if (bytes_written == -1 || bytes_written != bytes_read)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't write to fd %d\n", to_fd);
-                        exit(errno);
-                }
-        }
-        if (bytes_read == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't read from fd %d\n", from_fd);
-                exit(errno);
-        }
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", to_fd);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", from_fd);
+		exit(98);
+	}
 
-        return 0;
+	return (0);
 }
 
