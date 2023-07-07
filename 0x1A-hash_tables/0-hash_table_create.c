54c6fe9e06c450f57d3d0ff903f329ad41aa7062
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-07-07 16:51:15 -0700

    main

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
 	return (table);
