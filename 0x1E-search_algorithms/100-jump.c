e836c2fc48ef48147b98957af9c86265071013c2
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-11 13:58:43 -0700

    jump search algorithmn

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = sqrt(size), right = 0, left = 0, i;
 
+	if (array == NULL)
+		return (-1);
+
 	while (right < size)
 	{
 		printf("Value checked array[%li] = [%i]\n", right, array[right]);
